61ae1df87e6d62bf90de5ac4d0bc255dc8f8eed4
ly7799 <luojie7799@foxmail.com>
2025-03-10 17:14:16 +0800

    sso is ok

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ docker compose \
 -f docker-compose.postgres.yml \
 -f docker-compose.redis.yml \
 -f docker-compose.minio.yml \
+-f docker-compose.sso.yml \
 -f docker-compose.attachment.yml \
 -f docker-compose.outline.yml \
 --env-file .env \
